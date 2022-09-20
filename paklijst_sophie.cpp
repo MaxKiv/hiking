6ff885c3bfea964d9eb631acd368a69462a4a063
Max Kivits <maxkivits42@gmail.com>
2022-09-20 18:58:34 +0200

    initial

---
+++ paklijst_sophie.cpp
@@ -0,0 +1,55 @@
+Labello
+Veiligheidsspeld
+Rugtas
+Slaapzak
+Slaapmat
+Tent
+Kookstel + alcohol (of zonder?) 
+  ophalen gasfles
+  pannetje
+  schoonmaken
+  testen geheel
+Houten spateltje
+Plastic bestek
+Wandelstokken
+Wandelsokken
+  1 paar teensokken (sophie?)
+  1 paar goede 
+  2 paar semi matige
+Waterfilter + 0.75L flessen
+Plastic scheermes
+Wandelbroek
+Wandelshirt
+Zonnebrand
+Mini EHBO kit
+Thermo lagen
+  onderlichaam
+  thermoshirt
+Wandelschoenen
+Slippers
+Regenjack
+Camera en spul
+  Vind + check gopro
+Hoofdlamp
+Mobiel + lader
+USB Battery pack
+  vind en laad powerbank
+Sponsje
+Microvezeldoek
+Zonnebril
+Hoofddeksel
+Vuilniszak voor in tas
+Zakmes
+Oortjes + Muziek
+Pasport
+Pinpas
+Print tickets
+
+~~~ Over Nadenken? ~~~
+Ereader
+
+~~~ Sophie Decathlon ~~~
+Sporttape
+Zeil voor onder tent?
+Wandelstokken
+Zitmatjes
